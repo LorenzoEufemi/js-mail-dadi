b7d4d0ee5b2fa84ea5e3c6429432b61b1c0ca128
LorenzoEufemi <lorenzoeufemi@gmail.com>
2024-11-04 15:23:48 +0100

    feat : inizializzazione commit

---
+++ js/README.md
@@ -0,0 +1,14 @@
+Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Non è necessario provvedere alla validazione delle email.
+NON USARE INCLUDES!
+
+dati
+creare array con mail
+chiedere all'utente mail
+
+esecuzione
+se utente-mail é presente in mail allora puo accedere
+altrimenti non puo accedere
---
+++ js/script.js
@@ -0,0 +1,21 @@
+// DATI
+
+const email = ["lorenzo@gmail.com" , "giovanni@gmail.com" , "liberato@gmail.com"];
+const emailUser = prompt("Digita qui la tua e-mail:");
+
+// ESECUZIONE
+let emailSearch = false;
+
+for (let i = 0; i < email.lenght; i++) {
+
+    
+    if ( email[i] === emailUser) {
+    emailSearch = true
+    }
+ };
+ if (emailSearch = true ) {
+    console.log("Acceso consentito!")
+
+ } else {
+    console.log("Accesso negato, riprovare.")
+ };
